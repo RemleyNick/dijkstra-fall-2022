aeac94d07ea688a4531554789c25a96e2e8a7577
RemleyNick <remleynick@yahoo.com>
2022-09-30 13:57:38 -0600

    Set up function kebab to Camel

---
+++ in-class-exercises/1-intro-and-review/kebabToCamelCase.js
@@ -0,0 +1,16 @@
+//change-this-word gets converted to changeThisWord
+//day-month-year gets converted to dayMonthYear
+//ALL-Caps get converted to allCaps
+
+function kebabtoCamel(kebabString) {
+    let camelString;
+    
+    //loop kebabString
+    //if dash, don't in use it & the following char would be capital
+    //otherwise, copy to lowercase to camelString
+
+
+    return camelString
+
+
+}
